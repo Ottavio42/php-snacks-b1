bb1735ee303e1c1ebe9feaba91b598a0c446520d
Ottavio42 <ottaviog88@gmail.com>
2021-10-20 12:19:48 +0200

    snack3

---
+++ snack3/index.php
@@ -0,0 +1,71 @@
+<?php
+$posts = [
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+?>
+
+<ul>
+    <?php foreach ($posts as $event => $eventList ) { ?>
+    <li>
+         <ul>
+            <?php foreach ($eventList as $element) ?>
+            <li>
+                <h1><?php echo $element["title"];?></h1>
+                <h2><?php echo $element["author"];?></h2>
+                <p><?php echo $element["text"];?></p>
+            </li>
+        </ul>
+    </li>
+    <?php } ?>
+</ul>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
