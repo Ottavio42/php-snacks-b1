d9c8f445c2fa0f0eea87ed479d672b1371a55efc
Ottavio42 <ottaviog88@gmail.com>
2021-10-21 16:58:32 +0200

    fine-sback7

---
+++ snack7/data.php
@@ -0,0 +1,21 @@
+<?php
+$studenti = [
+    [
+        "nome" => "Ottavio",
+        "Cognome" => "Giudici",
+        "voti" => [6 , 5 , 5 , 7 , 6]
+    ],
+    [
+        "nome" => "Gennaro",
+        "Cognome" => "Rossi",
+        "voti" => [9 , 6 , 3 , 7 , 7]
+    ],
+    [
+        "nome" => "Marco",
+        "Cognome" => "Limoni",
+        "voti" => [2 , 8 , 8 , 4 , 5]
+    ],
+]
+
+
+?>
